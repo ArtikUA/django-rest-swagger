53df0684b8b31605ad64615abcea9394eeece22d
Artem Khomenko <artik.ua@gmail.com>
2017-01-23 15:56:05 +0200

    Update introspectors.py

--- rest_framework_swagger/introspectors.py
+++ rest_framework_swagger/introspectors.py
@@ -679,11 +679,8 @@ class ViewSetMethodIntrospector(BaseMethodIntrospector):
     @property
     def is_array_response(self):
         """ ViewSet.list methods always return array responses """
-        is_array = super(ViewSetMethodIntrospector, self).is_array_response
-        if is_array is False:
-            return False
-        elif is_array or self.method == 'list':
-            return True
+        return (self.method == 'list' or
+                super(ViewSetMethodIntrospector, self).is_array_response)
 
     def get_http_method(self):
         return self.http_method
